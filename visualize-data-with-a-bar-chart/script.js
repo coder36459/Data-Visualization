1f78973b8ac85f71ef195cba6e69d8f7ec66bebb
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-06-19 17:33:12 +0200

    Update script.js

--- visualize-data-with-a-bar-chart/script.js
+++ visualize-data-with-a-bar-chart/script.js
@@ -1,20 +1,40 @@
 "use strict";
 const source = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-
 function app(data) {
 	const dateValue = data;
 	const w = 800;
-	const h = 600;
+	const h = 460;
 	const p = 50;
-	const xScale = d3.scaleLinear().domain([0, d3.max(dateValue, (d, i) => i)]).range([p, w - p]);
-	const yScale = d3.scaleLinear().domain([0, d3.max(dateValue, (d) => d[1])]).range([h - p, p]);
+	const gdp = d3.max(dateValue, (d, i) => d[1]);
+	const tip = d3.select("body").append("div").attr("id", "tooltip");
+	const xScale = d3.scaleUtc().domain([d3.min(dateValue, (d, i) => new Date(d[0])), d3.max(dateValue, (d, i) => new Date(d[0]))]).range([p, w - p]);
+	const yScale = d3.scaleLinear().domain([0, gdp]).range([h - p, p]);
 	const xAxis = d3.axisBottom(xScale);
 	const yAxis = d3.axisLeft(yScale);
+	const dateScale = d3.scaleLinear().domain([0, (dateValue.length - 1)]).range([p, w - p]);
+	const gdpScale = d3.scaleLinear().domain([0, gdp]).range([0, h - p - p]);
 	const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
-	svg.selectAll("rect").data(dateValue).enter().append("rect").attr("class", "bar").attr("data-date", (d) => d[0]).attr("data-gdp", (d) => d[1])
-	svg.append("text").attr("id", "title")
-	svg.append("g").attr("transform", "translate(0, " + (h - p) + ")").attr("id", "x-axis").call(xAxis);
-	svg.append("g").attr("transform", "translate(" + p + ",0)").attr("id", "y-axis").call(yAxis);
+	svg.selectAll("rect").data(dateValue).enter().append("rect").attr("class", "bar").attr("fill", "violet")
+	.attr("width", 3).attr("data-date", (d, i) => d[0]).attr("x", (d, i) => dateScale(i))
+	.attr("height", (d, i) => gdpScale(d[1])).attr("data-gdp", (d, i) => d[1]).attr("y", (d, i) => h - p - gdpScale(d[1]))
+	.on("mouseover", (d, i) => {
+		let m = new Date(i[0]).getMonth(), q;
+		if (m == 0) {q = " Q1"};
+		if (m == 3) {q = " Q2"};
+		if (m == 6) {q = " Q3"};
+		if (m == 9) {q = " Q4"};
+		tip.transition().style("visibility", "visible");
+		tip.html(i[0] + q + "<br>" + new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(i[1]) + " Billion")
+		.style("left", (event.pageX + 16) + "px")
+		.style("top", (event.pageY + 7) + "px")
+		.attr("data-date", i[0]);
+		d.target.style.fill = "purple";
+	})
+	.on("mouseout", (d, i) => {
+		tip.transition().style("visibility", "hidden");
+		d.target.style.fill = "violet";
+	});
+	svg.append("g").attr("transform", "translate(0, " + (h - p) + ")").attr("id", "x-axis").attr("class", "tick").call(xAxis);
+	svg.append("g").attr("transform", "translate(" + p + ",0)").attr("id", "y-axis").attr("class", "tick").call(yAxis);
 }
-
-fetch(source).then(response => response.json()).then(data => {d3.select("body").append("div").attr("id", "title").text(data.name.substring(0, 22)).append("div").attr("id", "duration").text("From " + data.from_date + " to " + data.to_date);app(data.data)});
+fetch(source).then(response => response.json()).then(data => {const body = d3.select("body");body.append("div").attr("id", "title").text(data.name.substring(0, 22));body.append("div").attr("id", "duration").text("From " + data.from_date + " to " + data.to_date);app(data.data)});
